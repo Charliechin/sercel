da65a6b738d55b8abbce31f987491b187fa522ec
charliechin <charliedn@gmail.com>
2023-02-08 11:38:15 +0000

    delete signout from Upload text

--- src/components/UploadText/UploadText.tsx
+++ src/components/UploadText/UploadText.tsx
@@ -1,16 +1,13 @@
-import { FormEvent, useEffect, useState } from "react"
-import { IText } from "../../interfaces/IText"
+import { FormEvent, useState } from "react"
 import { API } from "aws-amplify"
-import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
+import { Button } from "@chakra-ui/react"
 
 
 const UploadText = () => {
   // put
   const [author, setAuthor] = useState('')
   const [textBody, setTextBody] = useState('')
-  // get
-  const [text, setText] = useState<IText[] | []>([])
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -23,25 +20,18 @@ const UploadText = () => {
 
   }
 
-  useEffect(() => {
-    API
-      .get('textAPI', '/texts/text', {})
-      .then(res => setText([...text, ...res]))
-  }, [text])
 
   return (
-    <Authenticator>
-      {({ signOut }) => (
-        <>
-          <form onSubmit={handleSubmit}>
-            <input value={author} placeholder="Jane Austen" onChange={(e) => setAuthor(e.target.value)} />
-            <input value={textBody} placeholder="Lorem Ipsum" onChange={(e) => setTextBody(e.target.value)} />
-            <button type="submit">Add text</button>
-            <button onClick={signOut}>Sign out</button>
-          </form>
-        </>
-      )}
-    </Authenticator>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={author} placeholder="Jane Austen" onChange={(e) => setAuthor(e.target.value)} />
+        <br />
+        <input value={textBody} placeholder="Lorem Ipsum" onChange={(e) => setTextBody(e.target.value)} />
+        <br />
+        <br />
+        <Button colorScheme="blue" type="submit">Add text</Button>
+      </form>
+    </>
   )
 }
 
