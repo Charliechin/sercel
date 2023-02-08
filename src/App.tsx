495069b8da55ccf931a5ebc735766aac2afa8267
Carlos Diaz <charliedn@gmail.com>
2023-02-08 02:15:52 +0000

    Add routes to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Amplify } from 'aws-amplify'
+import '@aws-amplify/ui-react/styles.css'
+import awsConfig from './aws-exports'
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import { Route, Routes } from 'react-router-dom';
+import Rules from './components/Rules/Rules';
+import Game from './components/Game/Game';
+import Home from './components/Home/Home';
+import Scoreboard from './components/Scoreboard/Scoreboard';
+import UploadText from './components/UploadText/UploadText';
 
+Amplify.configure(awsConfig)
+//REST API endpoint: https://nj36t2qvxf.execute-api.eu-west-1.amazonaws.com/dev
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/rules" element={<Rules />}></Route>
+        <Route path="/game" element={<Game />}></Route>
+        <Route path="/scoreboard" element={<Scoreboard />}></Route>
+        <Route path="/upload-text" element={<UploadText />}></Route>
+      </Routes>
+      <header className="App-header">Hello</header>
+      {/* TODO: Sign out btn */}
+
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
